3a7ccfe532d651b6d0552cfa2bf87c148045904c
shilp <shilp@DELL>
2022-12-17 17:09:10 +0530

    TC_216 commiited

--- src/test/java/createOrganizations/createOrg.java
+++ src/test/java/createOrganizations/createOrg.java
@@ -9,6 +9,15 @@ public class createOrg {
 	public void TC_215()
 	{
 		System.out.println("this is tc 215");
+		
 	}
+	
+	@Test
+	
+	public void TC_216()
+	{
+		System.out.println("this is tc 215");
+	}
+	
 
 }
